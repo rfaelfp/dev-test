8a0b7a9d39c16ee9e0798ce727e0933c9fcbc4ac
rfalefp <rafaelfepedrosa@gmail.com>
2021-06-17 22:58:11 -0300

    Tentativa de implementar o json-server no docker compose e inciando modelagem de outros componenetes

--- todolist/src/components/templates/Header.jsx
+++ todolist/src/components/templates/Header.jsx
@@ -1,6 +1,11 @@
 import './Header.css'
 import React from 'react'
+import Icon from '@material-ui/core/Icon';
 
 
-export default propos => 
-    <div className="header">My TODO List</div>
+export default propos =>
+    <div className="header">
+        <Icon className="fa-solid fa-book" />
+        My TODO List
+
+    </div>
